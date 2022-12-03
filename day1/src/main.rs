1fac111dff5e4d14a91c72d8d27123791b4700fb
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-03 11:38:26 -0800

    Day 1, part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     let args = Args::parse();
 
     let data = fs::read_to_string(args.data_file).expect("Failed to read file");
-    let floor: i32 = data
+    let transitions: Vec<i32> = data
         .chars()
         .map(|c| {
             if c == '(' {
@@ -23,7 +23,17 @@ fn main() {
                 0
             }
         })
-        .sum();
+        .collect();
 
-    println!("Floor: {}", floor);
+    println!("Floor: {}", transitions.iter().sum::<i32>());
+
+    // Part 2
+    let mut floor = 0;
+    for (pos, transition) in transitions.iter().enumerate() {
+        floor += transition;
+        if floor < 0 {
+            println!("Entered basement on {} character", pos + 1);
+            break;
+        }
+    }
 }
