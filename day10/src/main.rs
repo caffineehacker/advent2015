059e9f4d933976ce00e715ad3b9c7e46cf63a117
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-08 20:09:47 -0800

    Day 10, part 1

---
+++ day10/src/main.rs
@@ -0,0 +1,73 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    input: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let mut state = string_to_rle(args.input);
+    for step in 0..40 {
+        state = rle(state);
+        println!(
+            "Step {}, length {}",
+            step,
+            state.iter().map(|s| s.0).sum::<u32>()
+        );
+    }
+
+    println!("Length is {}", state.iter().map(|s| s.0).sum::<u32>());
+}
+
+fn string_to_rle(input: String) -> Vec<(u32, u32)> {
+    let mut index = 0;
+    let mut output = Vec::new();
+
+    while index < input.len() {
+        let number = input.chars().nth(index).unwrap();
+        let count = input
+            .chars()
+            .skip(index)
+            .take_while(|c| *c == number)
+            .count();
+
+        output.push((count as u32, number.to_string().parse::<u32>().unwrap()));
+        index += count;
+    }
+
+    output
+}
+
+fn rle(state: Vec<(u32, u32)>) -> Vec<(u32, u32)> {
+    let mut index = 0;
+    let mut output = Vec::new();
+
+    let state: Vec<(u32, u32)> = state
+        .iter()
+        .flat_map(|s| vec![(1u32, s.0), (1u32, s.1)])
+        .collect();
+
+    while index < state.len() {
+        let current = state[index];
+        // If current is (3, 1) -> "111" then the next output would be (1, 3), (1, 1) -> "31"
+        let new_count: u32 = state
+            .iter()
+            .skip(index)
+            .take_while(|c| c.1 == current.1)
+            .map(|s| s.0)
+            .sum();
+
+        output.push((new_count as u32, current.1));
+        index += state
+            .iter()
+            .skip(index)
+            .take_while(|c| c.1 == current.1)
+            .count();
+    }
+
+    output
+}
