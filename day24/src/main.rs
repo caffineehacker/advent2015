0cc56785c734695f35f5f839676ffccfae6710d8
Tim Waterhouse <tim@timwaterhouse.com>
2022-12-24 19:30:05 -0800

    Day 24, part 2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -25,10 +25,18 @@ fn main() {
         .map(|line| line.expect("Failed to read line").parse().unwrap())
         .collect();
 
-    let weight_third: i64 = weights.iter().sum::<i64>() / 3;
+    let part1 = solve(&args, &weights, 3);
+    println!("Part 1: {}", part1);
+
+    let part2 = solve(&args, &weights, 4);
+    println!("Part 2: {}", part2);
+}
+
+fn solve(args: &Args, weights: &Vec<i64>, compartments: i64) -> i64 {
+    let weight_fraction: i64 = weights.iter().sum::<i64>() / compartments;
 
     if args.debug {
-        println!("Weight third: {}", weight_third);
+        println!("Weight fraction: {}", weight_fraction);
     }
 
     let mut lowest_valid_entanglement = i64::MAX;
@@ -39,7 +47,7 @@ fn main() {
         for ws in weights
             .iter()
             .combinations(i)
-            .filter(|w| w.iter().cloned().sum::<i64>() == weight_third)
+            .filter(|w| w.iter().cloned().sum::<i64>() == weight_fraction)
         {
             let quantum_entanglement = ws.iter().fold(1, |acc, w| acc * **w);
             if quantum_entanglement > lowest_valid_entanglement {
@@ -53,21 +61,42 @@ fn main() {
                 );
             }
 
-            for j in 1..weights.len() {
-                if weights
-                    .iter()
-                    .filter(|w| !ws.contains(w))
-                    .combinations(j)
-                    .any(|lw| lw.iter().cloned().sum::<i64>() == weight_third)
-                {
-                    lowest_valid_entanglement = quantum_entanglement;
-                    break;
-                }
+            let unused_weights: Vec<&i64> = weights.iter().filter(|w| !ws.contains(w)).collect();
+            if any_valid_compartments(unused_weights, compartments - 1, weight_fraction) {
+                lowest_valid_entanglement = quantum_entanglement;
+                break;
             }
         }
         if lowest_valid_entanglement < i64::MAX {
-            println!("Part 1: {}", lowest_valid_entanglement);
-            break;
+            return lowest_valid_entanglement;
         }
     }
+
+    -1
+}
+
+fn any_valid_compartments(weights: Vec<&i64>, compartments: i64, weight_fraction: i64) -> bool {
+    for j in 1..(weights.len() - (compartments as usize - 1)) {
+        if weights.iter().combinations(j).any(|lw| {
+            if lw.iter().cloned().cloned().sum::<i64>() != weight_fraction {
+                return false;
+            }
+
+            // If there are only two compartments and one works then the other has to work
+            if compartments == 2 {
+                return true;
+            }
+
+            let weights = weights
+                .iter()
+                .filter(|w| !lw.contains(w))
+                .cloned()
+                .collect_vec();
+            return any_valid_compartments(weights, compartments - 1, weight_fraction);
+        }) {
+            return true;
+        }
+    }
+
+    false
 }
